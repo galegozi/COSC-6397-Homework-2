2702c96fe61fdd8d02eab897ccea40ff06f41b05
Gal Egozi <geegozi@gmail.com>
2021-02-25 11:17:17 -0600

    Adding and Implementing the Switch Button as well as adding some assets.

---
+++ Assets/Scripts/SwitchScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour
+{
+    //public UnityEngine.GameObject downloadedObject;
+    private GameObject downloaded;
+    private GameObject galIce;
+    private bool gal = true;
+    //public UnityEngine.GameObject galObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var comp = GetComponentsInChildren(typeof(Transform));
+        foreach(var c in comp)
+        {
+            Debug.Log(c.gameObject.name);
+            if(c.gameObject.name == "DownloadedBanana")
+            {
+                downloaded = c.gameObject;
+                downloaded.SetActive(false);
+            }
+            else if(c.gameObject.name == "GalIce")
+            {
+                galIce = c.gameObject;
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void switchButtonPushed()
+    {
+        gal = !gal;
+        galIce.SetActive(gal);
+        downloaded.SetActive(!gal);
+    }
+}
